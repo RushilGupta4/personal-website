28cf718bc42745692466a0865fd83927e4649617
Ruzil357 <rushilgupta4@gmail.com>
2023-09-11 10:13:38 +0530

    Better

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,87 @@
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineClockCircle } from "react-icons/ai";
+
+const t =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla varius eros quis justo consequat mattis. Vivamus eget metus aliquet, venenatis neque a, convallis mi. Praesent facilisis nibh diam, sed elementum diam sodales a. Nam in justo non velit fringilla eleifend sit amet a elit. Aliquam vitae cursus diam, quis porta turpis. Nullam gravida metus id quam consectetur, consectetur cursus dolor semper. Donec pretium sem eget porta laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porta est sapien, at elementum ligula aliquet in. Proin sodales nulla eu luctus scelerisque. Donec accumsan velit at ante mollis mollis.";
+const blogs = [
+  {
+    title: "How to make a blog",
+    body: t,
+    link: "/",
+    date: "12th October, 2021",
+    img: "/profile_pic.png",
+    imgDesc: "This is a description of the image",
+  },
+  {
+    title: "My Second Blog",
+    body: t,
+    link: "/",
+    date: "20th October, 2021",
+    img: "/profile_pic.png",
+    imgDesc: "This is a description of the image",
+  },
+  {
+    title: "Where I've Been",
+    body: t,
+    link: "/",
+    date: "30th September, 2021",
+    img: "/profile_pic.png",
+    imgDesc: "This is a description of the image",
+  },
+];
+
+blogs.reverse();
+
+const BlogPreview = ({
+  data,
+}: {
+  data: { title: string; body: string; link: string; date: string; img: string; imgDesc: string };
+}): React.ReactNode => {
+  return (
+    <div className={`mx-auto w-full sm:w-4/5 px-4 lg:px-8 py-4 border-t-[3px] lg:border-t-[4px] border-[#222d49] flex flex-col lg:flex-row`}>
+      <div className={`mx-auto w-2/3 lg:w-1/4 rounded-lg border border-black`}>
+        <Image
+          src={data.img}
+          alt={data.imgDesc}
+          sizes='100vw'
+          width={1}
+          height={1}
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          quality={100}
+        />
+      </div>
+      <div className={`flex flex-col justify-between pt-1 lg:pt-0 px-1 lg:px-4 w-full lg:w-3/4`}>
+        <div>
+          <p className={`text-[20px] md:text-[24px] font-bold`}>{data.title}</p>
+          <div className='flex items-center gap-x-1'>
+            <AiOutlineClockCircle />
+            <p className={`text-sm md:text-base text-gray-600 font-light pl-[1px]`}>{data.date}</p>
+          </div>
+          <p className={`text-[16px] md:text-[18px] font-light pt-3 line-clamp-3`}>{data.body}</p>
+        </div>
+        <Link target={"_blank"} href={data.link} className={"rounded-md p-3 bg-blue-500 text-white font-bold w-max"}>
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const BlogsPage = () => {
+  return (
+    <div id='blogs' className={"pt-4 mb-20 lg:pt-12 lg:px-0"}>
+      <p className={`base-heading text-[40px] sm:text-[54px]`}>Blogs</p>
+      <div className={`flex flex-col pt-2 lg:pt-4 gap-4 justify-center items-stretch`}>
+        {blogs.map((blogData) => (
+          <BlogPreview key={blogData.link} data={blogData} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BlogsPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,53 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
+import { SocialIcon } from "react-social-icons";
+import { ImBlogger2 } from "react-icons/im";
 
 export default function Home() {
   return (
-    <div className={`flex-none lg:flex pt-8 lg:px-0`}>
-      <div className={`mx-auto w-full lg:w-1/3`}>
-        <Image 
-        src='/profile_pic.png' 
-        width={275} 
-        height={275} 
-        alt="Picture of me" 
-        className={`mx-auto`}
-        />
+    <div className={"flex flex-col justify-between h-[60vh] lg:h-[65vh] pt-5 lg:pt-12 lg:px-0"}>
+      <div className={`flex-none lg:flex`}>
+        {/* Profile Pic */}
+        <div className={`mx-auto w-full lg:w-[35%]`}>
+          <div className={`mx-auto w-[65%] min-[540px]:w-[60%] sm:w-[55%] md:w-[40%] min-[920px]:w-[35%] lg:w-[85%]`}>
+            <Image
+              src='/profile_pic.jpeg'
+              sizes='100vw'
+              width={1}
+              height={1}
+              style={{
+                width: "100%",
+                height: "auto",
+              }}
+              quality={100}
+              alt='Picture of me'
+              className={`mx-auto border-8 border-slate-300 rounded-full`}
+            />
+          </div>
+        </div>
+
+        {/* About Me */}
+        <div className={`mx-auto w-full lg:w-[70%] pt-3 lg:pt-0 lg:pl-2`}>
+          <p
+            className={`base-heading underline decoration-[2px] sm:decoration-[3px] underline-offset-[8px] sm:underline-offset-[12px] text-4xl sm:text-6xl`}
+          >
+            Hi, I'm Rushil
+          </p>
+          <p className={`pt-3 lg:pt-8 font-extralight lg:font-[125] text-[18px] lg:text-[22px] text-center lg:text-left px-0 lg:px-4`}>
+            Hi, I'm Rushil Gupta, a 1st year Computer Science student. I'm currently researching on algorithmic trading.
+          </p>
+        </div>
       </div>
-      <div className={`mx-auto w-full lg:w-2/3 pt-4 sm:pt-8 md:pt-10 lg:pt-0 lg:pl-1`}>
-        <p className={`text-gray-600 font-bold text-center underline decoration-[3px] underline-offset-[10px] text-4xl sm:text-6xl sm:underline-offset-[20px]`}>
-          Hi, I'm Rushil
-        </p>
-        <p className={`pt-12 font-extralight text-xl lg:text-[22px] lg:font-[125] px-0 lg:px-4`}>
-          I'm a student developer at Ashoka University, set to graduate in 2023.
-          
-        </p>
+
+      {/* Socials */}
+      <div className={"flex gap-x-4 items-center justify-center pt-4 sm:pt-8 pb-2"}>
+        <SocialIcon target='_blank' url={"https://github.com/Ruzil357"} bgColor={"#01040D"} />
+        <SocialIcon target='_blank' url={"https://linkedin.com/in/rushilgupta4"} bgColor={"#0a66c2"} />
+        <SocialIcon network={"email"} url={"mailto:rushilgupta4@gmail.com"} bgColor={"#d53833"} />
+        <Link href={"/blogs"}>
+          <ImBlogger2 href={"/blogs"} className={"rounded-full border-[8px] border-black"} size={50} />
+        </Link>
       </div>
     </div>
-  )
+  );
 }
