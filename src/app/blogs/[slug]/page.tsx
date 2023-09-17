8b7c4698dd847bb5aa93230b898c876bffa94713
Ruzil357 <rushilgupta4@gmail.com>
2023-09-17 14:18:03 +0530

    Final Changes Done, Just Need to add blogs now

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -8,7 +8,7 @@ const getPageContent = async (slug: string) => {
 
 export async function generateMetadata({ params }: { params: any }) {
   const { meta }: { meta: any } = await getPageContent(params.slug);
-  return { title: meta.title };
+  return { title: `${meta.title} | Rushil Gupta` };
 }
 
 const Page = async ({ params }: { params: any }) => {
@@ -20,7 +20,9 @@ const Page = async ({ params }: { params: any }) => {
 
   return (
     <div className="pt-4 md:pt-8">
-      <article className="prose prose-base md:prose-lg lg:prose-xl prose-slate prose-headings:underline">{content}</article>
+      <article className="prose prose-base md:prose-lg lg:prose-xl prose-slate prose-h1:pb-1 md:prose-h1:pb-2 prose-h1:border-b-2 md:prose-h1:border-b-[3px] prose-h1:border-slate-700">
+        {content}
+      </article>
     </div>
   );
 };
